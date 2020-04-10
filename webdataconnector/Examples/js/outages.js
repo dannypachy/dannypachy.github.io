046dcf9529797290bb50c7b4f4dd0ef187592c9d
dannypachy <63383079+dannypachy@users.noreply.github.com>
2020-04-10 13:50:14 -0400

    Update outages.js

--- webdataconnector/Examples/js/outages.js
+++ webdataconnector/Examples/js/outages.js
@@ -1,15 +1,28 @@
-(function() {
-    // Create the connector object
-    var myConnector = tableau.makeConnector();
+"use strict";
 
-    // Init function for connector, called during every phase
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+(function () {
+    // Create the connector object
+    var myConnector = tableau.makeConnector(); // Init function for connector, called during every phase
+
     myConnector.init = function (initCallback) {
         tableau.authType = tableau.authTypeEnum.none;
         initCallback();
-    }
+    }; // Define the schema
 
-    // Define the schema
-    myConnector.getSchema = function(schemaCallback) {
+
+    myConnector.getSchema = function (schemaCallback) {
         var cols = [{
             id: "outageID",
             alias: "Outage ID",
@@ -55,51 +68,35 @@
             alias: "Equipment Constraint",
             dataType: tableau.dataTypeEnum.string
         }];
-
         var tableSchema = {
             id: "outages",
             alias: "All IESO Transmission Outages",
             columns: cols
         };
-
         schemaCallback([tableSchema]);
-    };
+    }; // Download the data
 
-    // Download the data
-    myConnector.getData = function(table, doneCallback) {
-        
+
+    myConnector.getData = function (table, doneCallback) {
         var outages = new Array();
         var xhttp = null;
-
-        const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const url = ["http://reports.ieso.ca/public/TxOutagesTodayAll/PUB_TxOutagesTodayAll.xml",
-            "http://reports.ieso.ca/public/TxOutages1to30DaysPlanned/PUB_TxOutages1to30DaysPlanned.xml",
-            "http://reports.ieso.ca/public/TxOutages31to90DaysPlanned/PUB_TxOutages31to90DaysPlanned.xml",
-            "http://reports.ieso.ca/public/TxOutages91to180DaysPlanned/PUB_TxOutages91to180DaysPlanned.xml",
-            "http://reports.ieso.ca/public/TxOutages181to730DaysPlanned/PUB_TxOutages181to730DaysPlanned.xml"]; // site that doesn’t send Access-Control-*
+        var proxyurl = "https://cors-anywhere.herokuapp.com/";
+        var url = ["http://reports.ieso.ca/public/TxOutagesTodayAll/PUB_TxOutagesTodayAll.xml", "http://reports.ieso.ca/public/TxOutages1to30DaysPlanned/PUB_TxOutages1to30DaysPlanned.xml", "http://reports.ieso.ca/public/TxOutages31to90DaysPlanned/PUB_TxOutages31to90DaysPlanned.xml", "http://reports.ieso.ca/public/TxOutages91to180DaysPlanned/PUB_TxOutages91to180DaysPlanned.xml", "http://reports.ieso.ca/public/TxOutages181to730DaysPlanned/PUB_TxOutages181to730DaysPlanned.xml"]; // site that doesn’t send Access-Control-*
 
         for (var k = 0; k < url.length; k++) {
-
-
             var x = new XMLHttpRequest();
+
             x.onloadend = function () {
                 if (this.readyState == 4 && this.status == 200) {
-
                     // document is ready:
                     xhttp = x.responseText;
-
                     parser = new DOMParser();
                     xmlDoc = parser.parseFromString(xhttp, "text/xml");
-
                     var temp = xmlDoc.getElementsByTagName("OutageRequest");
-
                     var i;
 
                     for (i = 0; i < temp.length; i++) {
-
-
                         var outage = {
-
                             outageID: temp[i].querySelector("OutageID").innerHTML,
                             plannedStart: temp[i].querySelector("PlannedStart").innerHTML,
                             plannedEnd: temp[i].querySelector("PlannedEnd").innerHTML,
@@ -112,41 +109,43 @@
                             equipmentvoltage: null,
                             constrainttype: null
                         };
-
                         var equipment = temp[i].querySelectorAll("EquipmentRequested");
-
                         var j;
 
                         for (j = 0; j < equipment.length; j++) {
-
                             var obj = Object.assign({}, outage);
                             obj.equipmentname = equipment[j].children[0].innerHTML;
                             obj.equipmenttype = equipment[j].children[1].innerHTML;
                             obj.equipmentvoltage = equipment[j].children[2].innerHTML;
                             obj.constrainttype = equipment[j].children[3].innerHTML;
                             outages.push(obj);
-                        };
-                    };
-                    outages = [... new Set(outages)];
-                };
+                        }
 
+                        ;
+                    }
+
+                    ;
+                    outages = _toConsumableArray(new Set(outages));
+                }
+
+                ;
             };
-            
+
             x.open('GET', proxyurl + url[k], false);
             x.send();
+        }
 
-        };
-
+        ;
         table.appendRows(outages);
         doneCallback();
     };
 
-    tableau.registerConnector(myConnector);
+    tableau.registerConnector(myConnector); // Create event listeners for when the user submits the form
 
-    // Create event listeners for when the user submits the form
-    $(document).ready(function() {
-        $("#submitButton").click(function() {
+    $(document).ready(function () {
+        $("#submitButton").click(function () {
             tableau.connectionName = "IESO Transmisson Outage Reports"; // This will be the data source name in Tableau
+
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
