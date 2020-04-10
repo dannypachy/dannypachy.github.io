67c3b7c8839983209bd7478c26f12e710db00ce8
dannypachy <63383079+dannypachy@users.noreply.github.com>
2020-04-10 13:26:51 -0400

    added init function

--- webdataconnector/Examples/js/outages.js
+++ webdataconnector/Examples/js/outages.js
@@ -2,6 +2,46 @@
     // Create the connector object
     var myConnector = tableau.makeConnector();
 
+    // Init function for connector, called during every phase but
+    // only called when running inside the simulator or tableau
+    myConnector.init = function (initCallback) {
+        tableau.authType = tableau.authTypeEnum.custom;
+
+        // If we are in the auth phase we only want to show the UI needed for auth
+        if (tableau.phase == tableau.phaseEnum.authPhase) {
+            $("#getvenuesbutton").css('display', 'none');
+        }
+
+        if (tableau.phase == tableau.phaseEnum.gatherDataPhase) {
+            // If the API that WDC is using has an endpoint that checks
+            // the validity of an access token, that could be used here.
+            // Then the WDC can call tableau.abortForAuth if that access token
+            // is invalid.
+        }
+
+        var accessToken = Cookies.get("accessToken");
+        console.log("Access token is '" + accessToken + "'");
+        var hasAuth = (accessToken && accessToken.length > 0) || tableau.password.length > 0;
+        updateUIWithAuthState(hasAuth);
+
+        initCallback();
+
+        // If we are not in the data gathering phase, we want to store the token
+        // This allows us to access the token in the data gathering phase
+        if (tableau.phase == tableau.phaseEnum.interactivePhase || tableau.phase == tableau.phaseEnum.authPhase) {
+            if (hasAuth) {
+                tableau.password = accessToken;
+
+                if (tableau.phase == tableau.phaseEnum.authPhase) {
+                    // Auto-submit here if we are in the auth phase
+                    tableau.submit()
+                }
+
+                return;
+            }
+        }
+    };
+
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
